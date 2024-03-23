137782c89391caf72848c278f5da4f16675b15d2
Claudio Corredoyra <ccorredoyra@gmail.com>
2024-03-23 20:23:42 -0300

    Prueba de envio de mail

--- sections/action_page.php
+++ sections/action_page.php
@@ -1,18 +1,37 @@
 <?php
-if (isset($_POST['enviar'])){
-    if(!empty($_POST['email'])){
-        $nombre = $_POST['nombre'];
-        $apellido = $_POST['apellido'];
-        $asunto = 'Información sobre Molfetta.'
-        $msg = 'Enviamos información para que pueda disfrutar este hermoso lugar.'
-        $email = $_POST['email'];
-        $header = "From: noreply@example.com" . "\r\n";
-        $header.= "Reply-to: noreply@example.com" . "\r\n";
-        $header.= "X-Mailer: PHP/". phpversion();
-        $mail = @mail($email0,$asunto,$msg,$header);
-        if ($mail){
-            echo "<h4> Mail enviado exitosamente ! </h4>";
-        }
-    }
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+date_default_timezone_set('America/Buenos Aires_City');
+
+header("application/json; charset=utf-8");  
+header("Cache-Control: no-cache");
+
+$data =$_POST['data'];
+
+function sendMail($data){
+	parse_str($data);
+// Para enviar un correo HTML mail, la cabecera Content-type debe fijarse
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+// Cabeceras adicionales
+$cabeceras .= 'To: '.utf8_decode('Informacion de Molfetta').' <noreplay@example.com>'. "\r\n";
+$cabeceras .= utf8_decode('From: Molfetta <noreplay@example.com>') . "\r\n";
+$cabeceras .= 'Cc: Clauker@gmail.com' . "\r\n";
+$titulo = utf8_decode("Información de Molfetta.");
+$msg = "Le enviamos la información para que pueda disfrutar de este hermoso lugar."
+
+mail($email,$titulo,$msg,$cabeceras);
+
 }
+
+sendMail($data);
+
+$ret = array();
+$ret[0]->msg =  "OK";
+$m = json_encode($ret);
+echo $m;
+
 ?>
