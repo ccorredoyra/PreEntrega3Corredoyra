4101c26f76670509c04f16bf1f17919eeed4dc9e
Claudio Corredoyra <ccorredoyra@gmail.com>
2024-03-24 02:05:52 -0300

    Envio de mail con PHP

--- sections/action_page.php
+++ sections/action_page.php
@@ -1,37 +1,41 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-date_default_timezone_set('America/Buenos Aires_City');
-
-header("application/json; charset=utf-8");  
-header("Cache-Control: no-cache");
-
-$data =$_POST['data'];
-
-function sendMail($data){
-	parse_str($data);
-// Para enviar un correo HTML mail, la cabecera Content-type debe fijarse
-$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
-$cabeceras .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-
-// Cabeceras adicionales
-$cabeceras .= 'To: '.utf8_decode('Informacion de Molfetta').' <noreplay@example.com>'. "\r\n";
-$cabeceras .= utf8_decode('From: Molfetta <noreplay@example.com>') . "\r\n";
-$cabeceras .= 'Cc: Clauker@gmail.com' . "\r\n";
-$titulo = utf8_decode("Información de Molfetta.");
-$msg = "Le enviamos la información para que pueda disfrutar de este hermoso lugar."
-
-mail($email,$titulo,$msg,$cabeceras);
-
+if (isset($_POST['enviar'])){
+    $email = $_POST['email'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $asunto = 'Información sobre Molfetta.';
+    $msg  = 'Sr/a. ';
+    $msg .= $nombre. "\r\n";
+    $msg .= $apellido;
+    $msg .= ', enviamos información para que pueda disfrutar este hermoso lugar.';
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $header = "From: noreply@example.com" . "\r\n";
+        $header.= "Reply-to: noreply@example.com" . "\r\n";
+        $header.= "X-Mailer: PHP/". phpversion();
+        $mail = mail($email, $asunto, $msg, $header);
+        if ($mail){
+            echo "<h4> Mail enviado exitosamente !!!</h4>";
+        }
+        else {
+            echo "<h4> Mail NO pudo ser enviado.</h4>"; 
+            echo $nombre. "\r\n";
+            echo $apellido. "\r\n";
+            echo $email. "\r\n" ;
+            echo $asunto. "\r\n";
+            echo $msg. "\r\n";
+            echo $header. "\r\n";
+            var_dump($mail);
+            
+        }
+    }
+    else {
+        echo "<h4> La dirección del eMail es inválida.</h4>"; 
+        echo $nombre. "\r\n";
+        echo $apellido. "\r\n";
+        echo $email. "\r\n" ;
+        echo $asunto. "\r\n";
+        echo $msg. "\r\n";
+    }
+    header("Location: informacion.html");      
 }
-
-sendMail($data);
-
-$ret = array();
-$ret[0]->msg =  "OK";
-$m = json_encode($ret);
-echo $m;
-
 ?>
